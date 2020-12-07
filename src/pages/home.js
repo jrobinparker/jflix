7b397525465a851eb300bfbeec2cdf9d62573d9d
Jeremy Parker <jrobinparker@gmail.com>
2020-12-06 19:32:07 -0800

    Added OptForm component to Header

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,13 +1,20 @@
 import React, { Fragment } from 'react';
+import { OptForm } from '../components';
+import FaqsContainer from '../containers/faqs';
 import JumbotronContainer from '../containers/jumbotron';
 import FooterContainer from '../containers/footer';
-import FaqsContainer from '../containers/faqs';
 import HeaderContainer from '../containers/header';
 
 export default function Home() {
   return (
     <Fragment>
-      <HeaderContainer />
+      <HeaderContainer>
+        <OptForm>
+          <OptForm.Input placeholder='Email Address' />
+          <OptForm.Button>Try it Now</OptForm.Button>
+          <OptForm.Text>Ready to watch? Enter your email address to create or restart your membership.</OptForm.Text>
+        </OptForm>
+      </HeaderContainer>
       <JumbotronContainer />
       <FaqsContainer />
       <FooterContainer />
