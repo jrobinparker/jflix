afd9ca522505bd0a40aba185066660d46d9eb92e
Jeremy Parker <jrobinparker@gmail.com>
2020-12-07 12:39:39 -0800

    Created SignIn component and updated routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import * as ROUTES from './constants/routes';
-import Home from './pages/home';
+import { Home, SignIn } from './pages';
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route exact path={ROUTES.HOME} component={Home} />
-        <Route path={ROUTES.SIGN_IN}>
-          <h1>sign in page</h1>
-        </Route>
+        <Route path={ROUTES.SIGN_IN} component={SignIn} />
         <Route path={ROUTES.SIGN_UP}>
           <h1>sign up page</h1>
         </Route>
---
+++ src/components/form/styles/form.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+import { Link as ReactRouterLink } from 'react-router-dom';
+
+export const Container = styled.section``;
+
+export const Base = styled.form``;
+
+export const Title = styled.h1``;
+
+export const Text = styled.p``;
+
+export const TextSmall = styled.p``;
+
+export const Input = styled.input``;
+
+export const Submit = styled.button``;
+
+export const Error = styled.div``;
+
+export const Link = styled(ReactRouterLink)``;
