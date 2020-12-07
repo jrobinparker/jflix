794a57c77f88f828d66175a9d1801d5f53de8d40
Jeremy Parker <jrobinparker@gmail.com>
2020-12-07 13:10:37 -0800

    Added Footer component to SignIn page

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { Fragment, useState } from 'react';
 import { Form } from '../components/';
 import HeaderContainer from '../containers/header';
+import FooterContainer from '../containers/footer';
 
 export default function SignIn() {
   const [ error, setError ] = useState('');
@@ -15,34 +16,37 @@ export default function SignIn() {
   }
 
   return (
-    <HeaderContainer>
-      <Form>
-        <Form.Title>Sign In</Form.Title>
-        {error && <Form.Error>{error}</Form.Error>}
-        <Form.Base onSubmit={handleSignIn} method='POST'>
-          <Form.Input
-            placeholder={emailAddress}
-            value={({ target }) => setEmailAddress((target.value))}
-          />
-          <Form.Input
-            type='password'
-            autoComplete='off'
-            placeholder={password}
-            value={({ target}) => setPassword(target.value)}
-          />
-          <Form.Submit
-            disabled={isValid}
-            type='submit'>
-            Sign In
-          </Form.Submit>
-          <Form.Text>
-            New to jflix? <Form.Link to='/signup'>Sign up now.</Form.Link>
-          </Form.Text>
-          <Form.TextSmall>
-            This page is protected by Google reCAPTCHA.
-          </Form.TextSmall>
-        </Form.Base>
-      </Form>
-    </HeaderContainer>
+    <Fragment>
+      <HeaderContainer>
+        <Form>
+          <Form.Title>Sign In</Form.Title>
+          {error && <Form.Error>{error}</Form.Error>}
+          <Form.Base onSubmit={handleSignIn} method='POST'>
+            <Form.Input
+              placeholder={emailAddress}
+              value={({ target }) => setEmailAddress((target.value))}
+            />
+            <Form.Input
+              type='password'
+              autoComplete='off'
+              placeholder={password}
+              value={({ target}) => setPassword(target.value)}
+            />
+            <Form.Submit
+              disabled={isValid}
+              type='submit'>
+              Sign In
+            </Form.Submit>
+            <Form.Text>
+              New to jflix? <Form.Link to='/signup'>Sign up now.</Form.Link>
+            </Form.Text>
+            <Form.TextSmall>
+              This page is protected by Google reCAPTCHA.
+            </Form.TextSmall>
+          </Form.Base>
+        </Form>
+      </HeaderContainer>
+      <FooterContainer />
+    </Fragment>
   )
 }
