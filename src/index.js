d87ab1349b90e450242ad232740d1347501945ce
Jeremy Parker <jrobinparker@gmail.com>
2020-12-07 11:04:23 -0800

    Hooked app up to Firebase

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { FirebaseContext } from './context/firebase';
 import { GlobalStyles } from './global-styles';
+
+const config = {
+    apiKey: "AIzaSyBlfjAN-dZHOW5EDVb_xl2i-OJas-TkmMU",
+    authDomain: "jflix-ac3e6.firebaseapp.com",
+    databaseURL: "https://jflix-ac3e6-default-rtdb.firebaseio.com",
+    projectId: "jflix-ac3e6",
+    storageBucket: "jflix-ac3e6.appspot.com",
+    messagingSenderId: "1078215977643",
+    appId: "1:1078215977643:web:073382fd204531f5cb65ee",
+    measurementId: "G-4M6Y2DD43Y"
+};
+
+const firebase = window.firebase.initializeApp(config);
+
 
 ReactDOM.render(
     <Fragment>
-      <GlobalStyles />
-      <App />
+      <FirebaseContext.Provider value={{ firebase: window.firebase }}>
+        <GlobalStyles />
+        <App />
+      </FirebaseContext.Provider>
     </Fragment>,
   document.getElementById('root')
 );
