435070214683d90389f3ab4ed8cce5699bc5c0a8
Jeremy Parker <jrobinparker@gmail.com>
2020-12-06 18:55:14 -0800

    Created Routes constant file

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import * as ROUTES from './constants/routes';
 import Home from './pages/home';
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route exact path='/' component={Home} />
-        <Route path='/signin'>
+        <Route exact path={ROUTES.HOME} component={Home} />
+        <Route path={ROUTES.SIGN_IN}>
           <h1>sign in page</h1>
         </Route>
-        <Route path='/signup'>
+        <Route path={ROUTES.SIGN_UP}>
           <h1>sign up page</h1>
         </Route>
-        <Route path='/browse'>
+        <Route path={ROUTES.BROWSE}>
           <h1>browse page</h1>
         </Route>
       </Switch>
